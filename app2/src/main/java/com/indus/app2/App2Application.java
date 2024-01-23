7d17d544f62f085ea885cb34dd5843c7e0cac235
Dayasagar159@ <dayasagar159@gmail.com>
2024-01-23 19:37:08 +0530

    Testing purpose

--- app2/src/main/java/com/indus/app2/App2Application.java
+++ app2/src/main/java/com/indus/app2/App2Application.java
@@ -11,5 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class App2Application {
     public static void main(String[] args) {
         SpringApplication.run(App2Application.class, args);
+        System.out.println("Application Started-->");
     }
 }
